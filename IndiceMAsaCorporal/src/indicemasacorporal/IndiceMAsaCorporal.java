96473d8a976a111f7d0a1ae328b5c5a0d0d464ef
AnakinVT0689 <Avaladez040@gmail.com>
2024-05-25 17:42:49 -0600

    Sube JAVA

---
+++ IndiceMAsaCorporal/src/indicemasacorporal/IndiceMAsaCorporal.java
@@ -0,0 +1,13 @@
+
+package indicemasacorporal;
+
+public class IndiceMAsaCorporal {
+
+    public static void main(String[] args) {
+        double peso ,estatura, indice;
+        
+        System.out.println("Ingrese por favor su peso en kg: ");
+        
+    }
+    
+}

aae8ed2c8e8481c0145242bfff818f41d1f983dc
AnakinVT0689 <Avaladez040@gmail.com>
2024-05-28 19:22:05 -0600

    Estructuras While

---
+++ EstructuraWhile2/src/estructurawhile2/EstructuraWhile2.java
@@ -0,0 +1,17 @@
+
+package estructurawhile2;
+
+public class EstructuraWhile2 {
+
+    public static void main(String[] args) {
+    int cont = 0;
+        
+        while (cont <= 10) {
+        
+            System.out.println("Esstoy en la vuelta "+ cont);
+            
+            cont = cont + 1;
+        }
+    }
+    
+}

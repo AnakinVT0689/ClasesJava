6305d89bec5a5e7ae07cc906c4cf5f9153ad3dc2
AnakinVT0689 <Avaladez040@gmail.com>
2024-06-06 19:31:10 -0600

    Estructuras repetitivas Vector

---
+++ ArregloVectorRepetitivo/src/main/java/com/mycompany/arreglovectorrepetitivo/ArregloVectorRepetitivo.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.arreglovectorrepetitivo;
+
+public class ArregloVectorRepetitivo {
+
+    public static void main(String[] args) {
+        //Declaracion
+        int vector [] = new int [4];
+        
+        //Asignacion (manual)
+        vector[0] = 2;
+        vector[1] = 35;
+        vector[2] = 48;
+        vector[3] = 157;        
+    }
+}

b8d506d3ef690f958eaa9a30827c86336310e12b
AnakinVT0689 <Avaladez040@gmail.com>
2024-06-18 18:50:49 -0600

    Ejecicio complementario Agenda Electronica

---
+++ EjercicioAgendaElectronica/src/ejercicioagendaelectronica/EjercicioAgendaElectronica.java
@@ -0,0 +1,14 @@
+
+package ejercicioagendaelectronica;
+
+import ejercicioagendaelectronica.igu.Principal;
+
+public class EjercicioAgendaElectronica {
+
+    public static void main(String[] args) {
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+    }
+    
+}
